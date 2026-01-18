482bec0eee698c271d224b203a04636ad2cbf676
Fernanda Ishida <fer.ishida@gmail.com>
2026-01-18 18:04:22 -0300

    Feat(admin): Add register to admin

--- week-1-django-rest/school/admin.py
+++ week-1-django-rest/school/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Student, Course
 
-# Register your models here.
+class Students(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'date_of_birth', 'email', 'cpf', 'phone_number',)
+    list_display_links = ('id', 'first_name', 'last_name',)
+    list_per_page = 20
+    search_fields = ('first_name',)
+
+admin.site.register(Student, Students)
+
+class Courses(admin.ModelAdmin):
+    list_display = ('id', 'course_id', 'description', 'level',)
+    list_display_links = ('id', 'course_id',)
+    search_fields = ('course_id',)
+
+admin.site.register(Course, Courses)
