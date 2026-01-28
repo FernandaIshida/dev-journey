124e4fe7c18dab3651ea78520e4f572aa2d59874
Fernanda Ishida <fer.ishida@gmail.com>
2026-01-28 18:02:21 -0300

    feat(docs): add swagger endpoints

--- week-1-django-rest/setup/urls.py
+++ week-1-django-rest/setup/urls.py
@@ -3,6 +3,21 @@ from django.urls import path, include
 from school.views import StudentViewSet, CourseViewSet, EnrollmentViewSet, EnrollmentStudentsList, EnrollmentCoursesList
 from rest_framework import routers
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Documentation",
+      default_version='v1',
+      description="API School Documentation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
 router = routers.DefaultRouter()
 router.register('students', StudentViewSet, basename='Students')
 router.register('courses', CourseViewSet, basename='Courses')
@@ -13,4 +28,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('students/<int:pk>/enrollments/', EnrollmentStudentsList.as_view()),
     path('courses/<int:pk>/enrollments/', EnrollmentCoursesList.as_view()),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
