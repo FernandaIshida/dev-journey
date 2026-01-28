e9ad5caec2444200e36c7a5a876483b5174fea4b
Fernanda Ishida <fer.ishida@gmail.com>
2026-01-28 16:20:03 -0300

    Feat(views): Add views to the project

--- week-1-practice/steam_match/views.py
+++ week-1-practice/steam_match/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from steam_match.models import Gamer, Game
+from steam_match.serializers import GamerSerializer, GameSerializer
+
+class GamerViewSet(viewsets.ModelViewSet):
+    queryset = Gamer.objects.all()
+    serializer_class = GamerSerializer
+    filter_backends = [DjangoFilterBackend,filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['nickname', 'join_date']
+    search_fields = ['nickname', 'steam_username', 'country']
+
+class GameViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    filter_backends = [DjangoFilterBackend,filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['title', 'release_date', 'playtime_minutes']
+    search_fields = ['title', 'genre', 'studio']        
